026f132b7c13ca7121e88729648c88d7bee26d71
James <JamesColinWilson@gmail.com>
2025-09-28 04:25:13 -0400

    Minor changes, to address bugs

--- future.R
+++ future.R
@@ -1,28 +1,8 @@
 library(reticulate)
-library(future)
-library(promises)
-plan(multicore)
+
 
 # Define any Python packages needed for the app here:
 PYTHON_DEPENDENCIES = 'yahooquery'
 
-
-my_future_function <- function(){
-  
-  fut <- future_promise({
-    expr <- quote({
-      use_virtualenv("tester", required = TRUE)
-    })
-    
-    if(virtualenv_exists(envname = "tester")){
-      eval(expr)
-    } else {
-      virtualenv_install("tester", packages = c(PYTHON_DEPENDENCIES))
-      eval(expr)
-    }
-  }) %...>% (function(result) {
-    return(result)
-  })
-
-  return(fut)
-}
+virtualenv_create(envname = 'tester', 
+                              python = '/usr/bin/python3')
